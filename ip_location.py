cf0438537d4068831af8b737af92bf08f93ed407
LeonardAntipuesto <leonard@example.com>
2022-10-27 04:06:13 +0000

    2nd commit

--- ip_location.py
+++ ip_location.py
@@ -4,17 +4,13 @@ from requests import get
 
 ip = get('https://api64.ipify.org').text
 # ip = get('https://ip6.seeip.org').text
-print('Your IPV4 address is: {}'.format(ip))
+print('Your IPV4 address is: {} \n'.format(ip))
 # print('Your IPV6 address is: {}'.format(ip2))
 
 response = requests.get("http://ip-api.com/json/"+ip).json()
-print("\nCountry: "+response['country'])
-print("Region Name: "+response['regionName'])
-print("City: "+response['city'])
-print("ZIP code: "+response['zip'])
-lat = str(response['lat'])
-print("Latitude: "+ lat)
-lon = str(response['lon'])
-print("Longitude: "+ lon)
-print("ISP: "+response['isp'])
-print("ASN: "+response['as'])
+x = response.items()
+d = {'status':'Status:', 'country':'Country:', 'countryCode': 'Country Code:', 'region':'Region:','regionName':'Region Name:','city':'City:', 'zip':'ZIP Code:','lat':'Latitude:','lon':'Longitude:','timezone':'Timezone:','isp':'ISP:', 'org':'Org:', 'as':'ASN:', 'query':'Query:'}
+change = dict((d[key], value) for (key, value) in x)
+for z in change:
+   print(z,change[z])
+
